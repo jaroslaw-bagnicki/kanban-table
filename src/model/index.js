93c480727827aef15d042beabef59864dc8d71f6
Jarosław Bagnicki <jarek@bagnicki.net>
2019-01-29 16:50:29 +0100

    Model refactor
    Squashed commit of the following:
    
    commit 33e7e2dc50d03599fb8b06a128b8c8df985b2c3c
    Author: Jarosław Bagnicki <jarek@bagnicki.net>
    Date:   Tue Jan 29 16:48:41 2019 +0100
    
        Model: Refactor storing data

--- src/model/index.js
+++ src/model/index.js
@@ -1,1 +1,12 @@
-export { default } from './Store';
+import { serializeBoard } from './Serializer';
+
+let board;
+
+export function initBoard(data) {
+  board = serializeBoard(data);
+  return board;
+}
+
+export function getBoard() {
+  return board;
+}
