bd751d82758a1fee2d7cadfa60d9b5b47e06eae6
Jarosław Bagnicki <jarek@bagnicki.net>
2019-01-29 13:40:48 +0100

    Some cleanup

--- src/service/index.js
+++ src/service/index.js
@@ -2,68 +2,6 @@ import storage from 'localforage';
 
 const STORAGE_KEY = 'kanban-board-demo';
 
-let startId = 0;
-function generateId() {
-  return startId += 1;
-}
-
-let startTimestamp = new Date(2019, 0, 19, 12).getTime();
-function generateDate() {
-  const increment = Math.round(Math.random() * 30 + 1) * 1000 * 60;
-  startTimestamp += increment;
-  return new Date(startTimestamp).toISOString();
-}
-
-const mockBoard = {
-  id: generateId(), 
-  name: 'Mock board',
-  createDate: generateDate(),
-  description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa, minima soluta. Optio non nisi facere.',
-  columns: [
-    {
-      id: generateId(), 
-      name: 'TODO',
-      createDate: generateDate(),
-      cards: [
-        {
-          id: generateId(), 
-          name: 'Task2',
-          createDate: generateDate(),
-          content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, sed.',
-          color: 'white'
-        },
-        {
-          id: generateId(), 
-          name: 'Task3',
-          createDate: generateDate(),
-          content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, voluptatibus odit?',
-          color: 'red'
-        }
-      ]
-    },
-    {
-      id: generateId(), 
-      name: 'WIP',
-      createDate: generateDate(),
-      cards: [
-        {
-          id: generateId(), 
-          name: 'Task1',
-          createDate: generateDate(),
-          content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid eum quod expedita.',
-          color: 'green'
-        }
-      ]
-    },
-    {
-      id: generateId(), 
-      name: 'DONE',
-      createDate: generateDate(),
-      cards: []
-    }
-  ]
-};
-
 async function getDataFromStorage() {
   try {
     return await storage.getItem(STORAGE_KEY);
@@ -94,19 +32,9 @@ async function removeDataFromStorage() {
   }
 }
 
-// Tests of storage
-// async function testStorage() {
-//   console.log(await setDataToStorage(mockBoard));
-//   console.log(await getDataFromStorage());
-//   console.log(await removeDataFromStorage());
-// }
-// testStorage();
-// console.log(mockBoard);
-
 export default {
   getData: getDataFromStorage,
   setData: setDataToStorage,
-  removeData: removeDataFromStorage,
-  mockBoard
+  removeData: removeDataFromStorage
 };
 
